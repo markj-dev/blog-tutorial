c9897ef543b70ac9797dfb16fd45b2168094f5ea
Mark Jackson <105235719+markj-dev@users.noreply.github.com>
2024-01-15 14:24:50 -0600

    added rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://chimerical-cuchufli-35a931.netlify.app"
+});
