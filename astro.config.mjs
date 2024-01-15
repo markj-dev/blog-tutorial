f88775578019fcfebffac7f6e3f570ebd1727a74
Mark Jackson <105235719+markj-dev@users.noreply.github.com>
2024-01-15 14:50:04 -0600

    added dark and light theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://chimerical-cuchufli-35a931.netlify.app"
+  site: "https://chimerical-cuchufli-35a931.netlify.app",
+  integrations: [preact()]
 });
